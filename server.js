e58e2d76d84d1ab45e0649f0e2fadbb634269f5b
Pradhan2017 <33887814+Pradhan2017@users.noreply.github.com>
2018-01-03 18:36:35 +0530

    update server file

--- server.js
+++ server.js
@@ -1,12 +1,4 @@
-var express = require('express');
-var app = express();
 
-var port = 3000;
-
-app.use(express.static(__dirname + '/'));
-app.listen(port);
-
-console.log('Server started at' + port);
 
 app.post('/api/login', function (req, res) {
   var mika = [{
